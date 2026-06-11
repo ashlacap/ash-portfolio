b7c92fe1f981b24acbf299e1e887ec217846074c
Ash <ashleynlacap@gmail.com>
2026-06-11 13:44:11 -0700

    Auto-save

--- src/AboutSection.jsx
+++ src/AboutSection.jsx
@@ -118,8 +118,8 @@ const VideoCard = ({ src, label, style: outerStyle = {} }) => {
         </div>
       )}
 
-      {/* Bottom control bar — shown on hover */}
-      {hovering && (
+      {/* Bottom control bar — shown on hover or when playing on touch */}
+      {(hovering || (isTouchDevice && playing)) && (
         <div
           onClick={e => e.stopPropagation()}
           style={{
