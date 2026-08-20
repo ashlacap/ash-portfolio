8f59507eb0cac40cd3fb1aaccfe16ca627a3e993
Ash <ashleynlacap@gmail.com>
2026-08-19 21:12:47 -0700

    Auto-save

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -49,7 +49,7 @@ const Footer = ({ onNavigate }) => {
             letterSpacing: '0.12em', textTransform: 'uppercase',
             color: '#3a4ea8', marginBottom: 16
           }}>Navigate</div>
-            {[['Work', 'work'], ['Projects', 'side-quests'], ['Videos', 'video-edits'], ['About', 'about']].map(([label, page]) =>
+            {[['Work', 'work'], ['Projects', 'side-quests'], ['About', 'about']].map(([label, page]) =>
           <div key={page} style={{ marginBottom: 10 }}>
                 <button onClick={() => onNavigate(page)} style={{
               fontFamily: "'DM Sans', sans-serif", fontSize: 14, fontWeight: 400,
