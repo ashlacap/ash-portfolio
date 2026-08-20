8e59c811aef9defd2d49b41e9425a0a1abba094a
Ash <ashleynlacap@gmail.com>
2026-08-19 20:00:14 -0700

    Auto-save

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -29,7 +29,6 @@ const Nav = ({ activePage, activeSection, onNavigate }) => {
   const links = [
     { label: 'Work', page: 'work' },
     { label: 'Products', page: 'side-quests' },
-    { label: 'Design', page: 'graphic-design' },
     { label: 'Videos', page: 'video-edits' },
     { label: 'About', page: 'about' },
   ];
